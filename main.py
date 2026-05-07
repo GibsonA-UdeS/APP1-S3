7f018374860e51eb4b4afc6b913b55b19bea2aad
GibsonA-UdeS <giba2984@usherbrooke.ca>
2026-05-06 20:43:48 -0400

    The best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from scipy.integrate import solve_ivp
+from scipy.integrate import solve_ivp, simpson
+from scipy.interpolate import interp1d
+from scipy.differentiate import derivative
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -10,13 +12,13 @@ Ixx = 0.6E-3            #kg * m²
 Iyy = 1.1E-3            #kg * m²
 Izz = 1.5E-3            #kg * m²
 L = 0.1                 #m
-V = 1                   #m/s
+V = 2                   #m/s
 alpha = 1E-4            #Ns²/m²
 rt = np.array([3,5,1])  #m
 
 to = 0                  #sec
 tf = 30                 #sec
-h = 0.1                 #pas d'intégration
+h = 0.5                 #pas d'intégration
 t_span = np.linspace(to, tf, int(((tf-to)/h)+1))
 
 F0 = (m*g)/4            #N
@@ -102,9 +104,35 @@ def drone_IED(t, var_etats):
 
   return [dxdt,dydt,dzdt,dvxdt,dvydt,dvzdt,dtheta_dt,dphi_dt,dpsi_dt,p_dot,q_dot,r_dot]
 
-sol = solve_ivp(drone_IED,t_span=[to,tf], y0=np.zeros(12), method='RK45',t_eval=t_span)
+def atterissage(t, y):
+    x, y, z = y[0], y[1], y[2]
+    dist_centre = ((x - 3)**2 + (y - 5)**2)**0.5
+    return z - 1 if dist_centre <= 1 else 1
 
-#print(sol.y[0], sol.y[1], sol.y[2])
+atterissage.terminal = True
+atterissage.direction = 0
+
+sol = solve_ivp(drone_IED,t_span=[to,tf], y0=np.zeros(12), method='RK45',t_eval=t_span,events=atterissage)
+
+if sol.status == 1:
+  print("Temps de trajet :", sol.t_events[0],"s")
+  print("Position d'atterissage :", sol.y_events[0][0][0:3],"m")
+else:
+  print("Atterissage manqué")
+  print("Position finale :", sol.y[0][-1], sol.y[1][-1], sol.y[2][-1], "m")
+
+longueur_traj = ((simpson(sol.y[3],sol.t))**2 + (simpson(sol.y[4],sol.t))**2 + (simpson(sol.y[5],sol.t))**2)**0.5
+print("Longueur de la trajectoire :", longueur_traj, "m")
+
+fx = interp1d(sol.t,sol.y[0],'cubic', fill_value="extrapolate")
+fy = interp1d(sol.t,sol.y[1],'cubic', fill_value="extrapolate")
+fz = interp1d(sol.t,sol.y[2],'cubic', fill_value="extrapolate")
+
+vit_instant = ((derivative(fx,sol.t).df)**2 + (derivative(fy,sol.t).df)**2 + (derivative(fz,sol.t).df)**2)**0.5
+plt.plot(sol.t, vit_instant, label='Vitesse instantanée')
+plt.xlabel('t (s)')
+plt.ylabel('v (m/s)')
+plt.legend()
 
 #Graphique 3d de l'évolution du marqueur
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -116,13 +144,11 @@ y1 = r * np.sin(theta1)
 z1 = np.zeros_like(theta1)
 ax.plot(x1+3, y1+5, z1+1, color='blue')
 
-ax.set(xticklabels=[],
-       yticklabels=[],
-       zticklabels=[])
 ax.scatter([3],[5],[1], color='r', label="Cible")
 ax.set_xlabel("x (m)")
 ax.set_ylabel("y (m)")
 ax.set_zlabel("z (m)")
 ax.set_title("Trajectoire 3D du drone")
+ax.grid(True)
 ax.legend()
 plt.show()
